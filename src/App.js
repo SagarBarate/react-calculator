920d71019eef90db7ebd207efb45abe8409c7232
Sagar Barate <sagarbarate2907@com.com>
2023-08-10 15:01:21 +0530

    buttons

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import './App.css';
 
 const  App =() => {
 
-  const [result, setResult] = useState("hii");
+  const [result, setResult] = useState();
 
   return (
    <>
      <div className='container'>
-      
+       <form>
+        <input type="text" value={result} />
+       </form>
+       <div className='keypad'>
+        <button>Clear</button>
+        <button>C</button>
+        <button>&divide;</button>
+        <button>7</button>
+        <button>8</button>
+        <button>9</button>
+        <button>&times;</button>
+        <button>4</button>
+        <button>5</button>
+        <button>6</button>
+        <button>&ndash;</button>
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+        <button>+</button>
+        <button>0</button>
+        <button>.</button>
+        <button>=</button>
+
+       </div>
      </div>
    </>
   );
